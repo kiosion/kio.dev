8f4f079fc62d23350fcb2f58c532139983101860
kiosion <hi@kio.dev>
2021-10-17 16:32:31 -0300

    Remove FA

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue' // Import main view
 import './assets/tailwind.css' // Import tailwind CSS
 
-// TODO: Fontawesome
-
-// import the fontawesome core
-//import { library } from '@fortawesome/fontawesome-svg-core'
-// import specific icons
-//import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-// import font awesome icon component
-//import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// add icons to the library
-//library.add(faUserSecret)
-// add font awesome icon component
-//Vue.component('font-awesome-icon', FontAwesomeIcon)
-//Vue.config.productionTip = false
-
 // const routes = [
 //     //TODO
 // ]
