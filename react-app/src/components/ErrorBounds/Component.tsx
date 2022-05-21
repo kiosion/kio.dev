b849e38916e3194915340ea1c2c842a88eacbfd4
kiosion <hi@kio.dev>
2022-05-21 17:49:35 -0300

    Various changes
    Start moving variables to scss, start new styles dir structure, update lint config temporarily

--- react-app/src/components/ErrorBounds/Component.tsx
+++ react-app/src/components/ErrorBounds/Component.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './Component.scss';
+//import './Component.scss';
 export default function Component() {
   return (
     <div className="app__errorWrapper-component">
