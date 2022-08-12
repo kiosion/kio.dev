125a425cd57f79ba2f78d0e03ad6aa56ada25b68
kiosion <hi@kio.dev>
2022-08-12 17:10:17 -0300

    Add netlify adapter

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-node';
+import NodeAdapter from '@sveltejs/adapter-node';
+import NetlifyAdapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter({ out: './dist' })
+    adapter:
+      process.env.SVELTE_ADAPTER_ENV === 'development'
+        ? NodeAdapter({ out: './dist' })
+        : NetlifyAdapter()
   }
 };
 
