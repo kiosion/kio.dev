006ff5bd51734c19db293627a432a1f107b80625
kiosion <hi@kio.dev>
2022-08-24 15:49:36 -0300

    Update env, upgrade sanity

--- svelte-app/src/lib/env.ts
+++ svelte-app/src/lib/env.ts
@@ -3,7 +3,9 @@ export const BASE_URL = '/';
 export const API_URL = `${BASE_URL}api/`;
 export const REMOTE_API_VERSION = 'v1';
 export const REMOTE_API_URL =
-  ENV === 'production' || ENV === 'backed'
+  ENV === 'production'
     ? `https://kio.sh/api/${REMOTE_API_VERSION}/`
-    : `http://localhost:4000/api/${REMOTE_API_VERSION}/`;
+    : ENV === 'backed'
+      ? `https://dev.kio.sh/api/${REMOTE_API_VERSION}/`
+      : `http://localhost:4000/api/${REMOTE_API_VERSION}/`;
 export const REMOTE_API_TOKEN = import.meta.env.VITE_API_TOKEN;
