e7b952615d73c90bd8084a0895c4cbc51de57a3e
kiosion <hi@kio.dev>
2022-11-03 23:06:39 -0300

    chore: Update envs, workflows

--- svelte-app/src/lib/env.ts
+++ svelte-app/src/lib/env.ts
@@ -6,15 +6,17 @@ export const REMOTE_API_URL =
   ENV === 'production'
     ? `https://kio.sh/api/${REMOTE_API_VERSION}`
     : ENV === 'backed'
-      ? `https://dev.kio.sh/api/${REMOTE_API_VERSION}`
-      : `http://localhost:4444/api/${REMOTE_API_VERSION}`;
+    ? `https://dev.kio.sh/api/${REMOTE_API_VERSION}`
+    : `http://localhost:4444/api/${REMOTE_API_VERSION}`;
 export const REMOTE_CDN_URL =
   ENV === 'production'
     ? 'https://kio.sh/cdn'
     : ENV === 'backed'
-      ? 'https://dev.kio.sh/cdn'
-      : 'http://localhost:4444/cdn';
+    ? 'https://dev.kio.sh/cdn'
+    : 'http://localhost:4444/cdn';
 export const REMOTE_API_TOKEN =
-  ENV === 'production' ? import.meta.env.VITE_API_TOKEN : '1234567890';
+  ENV === 'production'
+    ? import.meta.env.VITE_API_TOKEN
+    : import.meta.env.VITE_DEV_API_TOKEN;
 export const SANITY_DATASET = ENV === 'production' ? 'production' : 'dev';
 export const SANITY_PROJECT_ID = 'gkgnfulv';
