d67b217c8ec43854a785d3f1050ab434accc7f07
kiosion <hi@kio.dev>
2022-12-18 03:26:19 -0400

    feat: Add section for extLinks on projects

--- svelte-app/src/components/document/content/common/footer.svelte
+++ svelte-app/src/components/document/content/common/footer.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
   import Divider from '$components/divider.svelte';
   import IconHeader from '$components/headings/icon-header.svelte';
-  import type { PostDocument, ProjectDocument } from '$types';
-  import { t } from '$i18n';
+  import Hoverable from '$components/hoverable.svelte';
   import Tags from '$components/tags.svelte';
+  import { t } from '$i18n';
+  import type { PostDocument, ProjectDocument } from '$types';
 
   export let model: 'post' | 'project';
   export let data: PostDocument | ProjectDocument;
+
+  $: extLinks =
+    model === 'project' &&
+    ((data as ProjectDocument)
+      .externalLinks as ProjectDocument['externalLinks'] &
+      {
+        hovered: boolean;
+      }[]);
 </script>
 
 <div class="mt-4" data-test-id="{model}-footer">
@@ -19,4 +28,49 @@
     />
     <Tags {model} data={data.tags} size="lg" />
   {/if}
+  {#if extLinks && extLinks.length > 0}
+    <Divider />
+    <IconHeader
+      icon="link"
+      text={t('Links')}
+      classes="mt-8 mb-4 w-full h-fit"
+    />
+    <ul class="list-disc ml-8">
+      {#each extLinks as link}
+        <li>
+          <Hoverable bind:hovered={link.hovered}>
+            <a
+              href={link.url}
+              target={'_blank'}
+              rel={'noopener noreferrer'}
+              class="underlined from-emerald-400 dark:from-emerald-300 {link.hovered
+                ? 'active dark:text-slate-800'
+                : ''} rounded-sm focusOutline-sm px-[2px] -mx[2px]"
+              tabindex="0"
+            >
+              {link.title}
+            </a>
+          </Hoverable>
+        </li>
+      {/each}
+    </ul>
+  {/if}
 </div>
+
+<style lang="scss">
+  .underlined {
+    text-decoration: none;
+    background-image: linear-gradient(
+      to right,
+      var(--tw-gradient-from) 0%,
+      var(--tw-gradient-from) 100%
+    );
+    background-position: bottom center;
+    background-repeat: no-repeat;
+    background-size: calc(100% - 4px) 2px;
+    transition: background-size 50ms ease, color 50ms ease;
+    &.active {
+      background-size: calc(100% - 4px) 100%;
+    }
+  }
+</style>
