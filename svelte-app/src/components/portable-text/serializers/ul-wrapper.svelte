4da67a8e1b993f7b578811d9670f31b31cd691a9
kiosion <hi@kio.dev>
2022-12-17 21:26:24 -0400

    fix: ul styles

--- svelte-app/src/components/portable-text/serializers/ul-wrapper.svelte
+++ svelte-app/src/components/portable-text/serializers/ul-wrapper.svelte
@@ -6,6 +6,13 @@
   $: ({ value: _value } = portableText);
 </script>
 
-<ul class="list-disc ml-6 leading-8">
+<ul class="pt-ul list-disc ml-6">
   <slot />
 </ul>
+
+<style>
+  :global(.pt-ul li) {
+    margin-bottom: 0.5rem;
+    line-height: 1.4rem;
+  }
+</style>
