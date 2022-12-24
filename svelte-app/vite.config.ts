f95bf237bd6f39b94f73bef0da5296979966e6d0
kiosion <hi@kio.dev>
2022-12-24 16:22:45 -0400

    fix: don't use commonjs transform, fix large chunk sizes

--- svelte-app/vite.config.ts
+++ svelte-app/vite.config.ts
@@ -3,7 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import StripTestSelectors from 'vite-plugin-test-selectors';
 import Inspect from 'vite-plugin-inspect';
 import svg from '@poppanator/sveltekit-svg';
-import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 
 export default defineConfig(({ command, mode }) => {
@@ -21,8 +20,8 @@ export default defineConfig(({ command, mode }) => {
       }),
       (isDev || isTesting) && Inspect(),
       // compile to good ol' ES5-compatible code
-      isProduction && commonjs(),
-      isProduction &&
+      (isProduction || isTesting) &&
+        command === 'build' &&
         babel({
           extensions: ['.js', '.mjs', '.html', '.svelte'],
           runtimeHelpers: true,
@@ -31,7 +30,7 @@ export default defineConfig(({ command, mode }) => {
             [
               '@babel/preset-env',
               {
-                targets: '> 0.5%, not dead, not ie 11'
+                targets: 'defaults, last 3 versions and not dead and not IE 11'
               }
             ],
             '@babel/preset-typescript'
@@ -44,16 +43,12 @@ export default defineConfig(({ command, mode }) => {
     ],
     optimizeDeps: {
       include: [
+        'svelte-highlight',
         'highlight.js',
         'highlight.js/lib/core',
-        'svelte-highlight',
         'seedrandom',
-        'moment',
         '@sanity/image-url'
       ]
-    },
-    ssr: {
-      noExternal: ['svelte-highlight', 'highlight.js', 'highlight.js/lib/core']
     },
     test: {
       globals: true,
@@ -65,7 +60,32 @@ export default defineConfig(({ command, mode }) => {
     build: {
       rollupOptions: {
         output: {
-          manualChunks: undefined
+          inlineDynamicImports: false,
+          manualChunks: (id: string) => {
+            // check for svelte-highlight / highlight.js and portabletext,
+            // chunk into their own dirs
+            switch (true) {
+              case id.includes('svelte-highlight/styles'):
+                return 'svelte-highlight/styles';
+              case id.includes('highlight.js/lib/core'):
+                return 'highlight.js/core';
+              case id.includes('@portabletext'):
+                return 'portabletext';
+            }
+            // check for hljs langs, chunk into dirs based on first letter
+            // to reduce size of chunks
+            const matchHlLang = id.match(
+                /highlight\.js(?:\/lib){0,1}\/languages\/([a-zA-Z])+/
+              ),
+              matchSHlLang = id.match(
+                /svelte-highlight(?:\/lib){0,1}\/languages\/([a-zA-Z])+/
+              );
+            if (matchHlLang) {
+              return `highlight.js/languages/${matchHlLang[1]}`;
+            } else if (matchSHlLang) {
+              return `svelte-highlight/languages/${matchSHlLang[1]}`;
+            }
+          }
         }
       }
     },
