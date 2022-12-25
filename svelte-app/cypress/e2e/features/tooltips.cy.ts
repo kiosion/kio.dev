a2aa6a07230b684607d47398815795f3a0e62014
kiosion <hi@kio.dev>
2022-12-24 22:39:04 -0400

    fix: Cy tooltip test

--- svelte-app/cypress/e2e/features/tooltips.cy.ts
+++ svelte-app/cypress/e2e/features/tooltips.cy.ts
@@ -21,7 +21,7 @@ describe('E2E | features | tooltips', () => {
   });
 
   it('should follow the cursor', () => {
-    cy.get('[data-test-id="nav logo"]')
+    cy.get('[data-test-id="nav-logo"]')
       .filter(':visible')
       .trigger('mouseenter');
     cy.wait(1000);
@@ -30,7 +30,7 @@ describe('E2E | features | tooltips', () => {
     // check that the tooltip follows the cursor, first store the initial position
     cy.get('[data-tippy-root]').then(($el) => {
       const initialPosition = $el[0].getBoundingClientRect();
-      cy.get('[data-test-id="nav logo"]')
+      cy.get('[data-test-id="nav-logo"]')
         .filter(':visible')
         // move mouse by 2px so it's still over the button
         .trigger('mousemove', { clientX: 2, clientY: 2 });
