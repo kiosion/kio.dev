de6c97994ed1435a077b0f21e9c0855fa93bfc42
kiosion <hi@kio.dev>
2023-01-14 15:55:20 -0400

    chore: Update Svelte, remove leftover `error-bound` code
    - Update Svelte from custom patch to v3.55.1
    - Remove leftover `error-bound` code
    - Fix some type imports

--- svelte-app/src/lib/error-bound/error-bound.ts
+++
@@ -1,6 +0,0 @@
-declare module './error-bound.svelte';
-import Component from './error-bound.svelte';
-import { createErrorBoundary } from './create-error-bound';
-
-// @ts-expect-error Bit of a hack
-export default createErrorBoundary(Component); // eslint-disable-line @typescript-eslint/no-unsafe-argument
