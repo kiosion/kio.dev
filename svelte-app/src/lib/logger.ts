05fd6ba3c4528d6d533a19fee40cbb509879bb93
kiosion <hi@kio.dev>
2023-06-13 17:21:29 -0300

    chore: Clean up logger.ts

--- svelte-app/src/lib/logger.ts
+++ svelte-app/src/lib/logger.ts
@@ -1,104 +1,59 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-function, prefer-rest-params */
 import { ENV } from '$lib/env';
 
-import type { LogOptions } from 'vite';
+const canLog = ENV !== 'production',
+  prevErrors = new Set(),
+  prevWarns = new Set();
 
-export class LoggerClass {
-  canLog!: boolean;
-  prevErrors!: Set<string>;
-  prevWarns!: Set<string>;
-  hasWarned!: boolean;
+const addTimestamp = (s: string) =>
+  `[${new Date().toLocaleTimeString('en-CA', { hour12: false })}] ${s}`;
 
-  constructor({ ENV }: { ENV: string }) {
-    this.canLog = ENV !== 'production';
-    this.prevErrors = new Set();
-    this.prevWarns = new Set();
-    this.hasWarned = false;
+const log = (msg: string, ...rest: any[]) => {
+  console.log(addTimestamp(msg), ...rest);
+};
+
+const info = (msg: string, ...rest: any[]) => {
+  if (!canLog) {
+    return;
   }
+  console.info(addTimestamp(msg), ...rest);
+};
 
-  #addTimestamp(msg: string) {
-    return `${new Date().toLocaleTimeString()} ${msg}`;
+const warn = (msg: string, ...rest: any[]) => {
+  if (!canLog) {
+    return;
   }
+  console.warn(addTimestamp(msg), ...rest);
+  prevWarns.add(`${msg}${JSON.stringify(rest)}`);
+};
 
-  log(msg: string, options: LogOptions = {}, ...rest: any[]) {
-    if (!this.canLog) {
-      return;
-    }
-    const { clear, timestamp } = options ?? {
-      clear: false,
-      timestamp: false
-    };
-    if (clear) {
-      console.clear();
-    }
-    msg = timestamp ? this.#addTimestamp(msg) : msg;
-    console.log(msg, ...rest);
+const error = (msg: string, ...rest: any[]) => {
+  if (!canLog) {
+    return;
   }
+  console.error(addTimestamp(msg), ...rest);
+  prevErrors.add(`${msg}${JSON.stringify(rest)}`);
+};
 
-  info(msg: string, options: LogOptions = {}, ...rest: any[]) {
-    if (!this.canLog) {
-      return;
-    }
-    const { clear, timestamp } = options ?? {
-      clear: false,
-      timestamp: false
-    };
-    if (clear) {
-      console.clear();
-    }
-    msg = timestamp ? this.#addTimestamp(msg) : msg;
-    console.info(msg, ...rest);
+const warnOnce = (msg: string, ...rest: any[]) => {
+  if (!canLog || prevWarns.has(`${msg}${JSON.stringify(rest)}`)) {
+    return;
   }
+  warn(msg, ...rest);
+};
 
-  warn(msg: string, options: LogOptions = {}, ...rest: any[]) {
-    if (!this.canLog) {
-      return;
-    }
-    const { clear, timestamp } = options ?? {
-      clear: false,
-      timestamp: false
-    };
-    if (clear) {
-      console.clear();
-    }
-    msg = timestamp ? this.#addTimestamp(msg) : msg;
-    this.hasWarned = true;
-    this.prevWarns.add(msg);
-    console.warn(msg, ...rest);
+const errorOnce = (msg: string, ...rest: any[]) => {
+  if (!canLog || prevErrors.has(`${msg}${JSON.stringify(rest)}`)) {
+    return;
   }
+  error(msg, ...rest);
+};
 
-  error(msg: string, options: LogOptions = {}, ...rest: any[]) {
-    const { clear, timestamp } = options ?? {
-      clear: false,
-      timestamp: false
-    };
-    if (clear) {
-      console.clear();
-    }
-    msg = timestamp ? this.#addTimestamp(msg) : msg;
-    this.prevErrors.add(msg);
-    console.error(msg, ...rest);
-  }
-
-  warnOnce(msg: string, options: LogOptions = {}, ...rest: any[]) {
-    if (!this.canLog) {
-      return;
-    }
-    if (this.prevWarns.has(msg)) {
-      return;
-    }
-    this.warn(msg, options, ...rest);
-  }
-
-  errorOnce(msg: string, options: LogOptions = {}, ...rest: any[]) {
-    if (!this.canLog) {
-      return;
-    }
-    if (this.prevErrors.has(msg)) {
-      return;
-    }
-    this.error(msg, options, ...rest);
-  }
-}
-
-export default new LoggerClass({ ENV });
+export default {
+  log,
+  info,
+  warn,
+  error,
+  warnOnce,
+  errorOnce
+};
