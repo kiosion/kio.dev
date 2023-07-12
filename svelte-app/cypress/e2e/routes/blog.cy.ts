c703b7f97b62a9ecfa02cfd645348e3eb57f6e0c
kiosion <hi@kio.dev>
2023-07-12 18:09:47 -0300

    chore: Remove unused test fixtures

--- svelte-app/cypress/e2e/routes/blog.cy.ts
+++ svelte-app/cypress/e2e/routes/blog.cy.ts
@@ -35,32 +35,5 @@ describe('E2E | Blog', () => {
     cy.get('[data-test-id="loader-full"]', { timeout: 6000 }).should('not.exist');
   });
 
-  // TODO: Re-enable after proper stubbing of api.
-  // it('should render blog route with no posts', () => {
-  //   setupContext({ num: 0 }).as('getPosts');
-  //   setupContext({ num: 0, many: false }).as('getPost');
-
-  //   cy.visit('/blog');
-  //   cy.wait('@getPosts');
-  //   cy.wait('@getPost');
-
-  //   cy.get('div.main', { timeout: 4000 }).should('exist');
-  //   cy.get('div.main').should('contain', 'kio.dev | Thoughts');
-
-  //   cy.get('[data-test-id="list-item"]').should('have.length', 0);
-  //   cy.get('div.main').should('contain', 'Hm, it seems empty around here');
-  // });
-
-  // it('should render blog route with posts', () => {
-  //   setupContext({ num: 10 }).as('getPosts');
-  //   setupContext({ num: 0, many: false }).as('getPost');
-
-  //   cy.visit('/blog');
-  //   cy.wait('@getPosts');
-  //   cy.wait('@getPost');
-
-  //   cy.get('div.main', { timeout: 4000 }).should('exist');
-  //   cy.get('div.main').should('contain', 'kio.dev | Thoughts');
-  //   cy.get('[data-test-id="list-item"]').should('have.length', 10);
-  // });
+  // TODO: Re-write after proper stubbing of api.
 });
