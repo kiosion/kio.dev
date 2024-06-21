408a6d366aba67ca24efb2aa47dea975c750ef0d
kiosion <hi@kio.dev>
2024-06-21 12:44:56 -0300

    feat: Resizable main container
    - Allow resizing main content area via drag-handles

--- svelte-app/src/components/layouts/scroll-container.svelte
+++ svelte-app/src/components/layouts/scroll-container.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { onDestroy, onMount } from 'svelte';
 
+  import { classList } from 'svelte-body';
+
   import { afterNavigate } from '$app/navigation';
   import { BASE_TRANSITION_DURATION } from '$lib/consts';
 
@@ -43,11 +45,81 @@
       });
     }, BASE_TRANSITION_DURATION);
   });
+
+  const DEFAULT_MAX_WIDTH_PX = 1216, // 76rem
+    DEFAULT_MIN_WIDTH_PX = 682;
+
+  let mouseDown: 'r' | 'l' | false = false,
+    handleHovered: 'r' | 'l' | false = false,
+    maxWidthPx = DEFAULT_MAX_WIDTH_PX;
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!mouseDown) {
+      return;
+    }
+
+    e.preventDefault();
+
+    switch (mouseDown) {
+      case 'r':
+        if (maxWidthPx + e.movementX * 2 < DEFAULT_MIN_WIDTH_PX) {
+          maxWidthPx = DEFAULT_MIN_WIDTH_PX;
+          break;
+        }
+        maxWidthPx += e.movementX * 2;
+        break;
+      case 'l':
+        if (maxWidthPx - e.movementX * 2 < DEFAULT_MIN_WIDTH_PX) {
+          maxWidthPx = DEFAULT_MIN_WIDTH_PX;
+          break;
+        }
+        maxWidthPx -= e.movementX * 2;
+        break;
+    }
+  };
 </script>
 
+<svelte:window
+  on:mouseup={() => (mouseDown = false)}
+  on:mousemove={handleMouseMove}
+  on:blur={() => ((mouseDown = false), (handleHovered = false))}
+/>
+
+<svelte:body
+  use:classList={{ 'select-none': mouseDown, 'cursor-col-resize': mouseDown }}
+/>
+
 <div
-  class="relative mx-auto flex h-full w-full max-w-[76rem] flex-col rounded-sm border border-dark/80 bg-neutral-50 shadow-lg shadow-dark/10 transition-colors dark:border-light/60 dark:bg-black dark:shadow-black/40"
+  class="relative mx-auto flex h-full w-full flex-col rounded-sm border border-dark/80 bg-neutral-50 shadow-lg shadow-dark/10 transition-colors dark:border-light/60 dark:bg-black dark:shadow-black/40"
+  style="max-width: {maxWidthPx}px;"
 >
+  <!-- right drag-handle -->
+  <div
+    class="absolute -right-2 bottom-0 top-0 hidden w-4 cursor-col-resize md:block"
+    on:mousedown={() => (mouseDown = 'r')}
+    on:mouseenter={() => (handleHovered = 'r')}
+    on:mouseleave={() => (handleHovered = false)}
+    role="presentation"
+  />
+  <div
+    class="absolute -right-2 bottom-1 top-1 w-0.5 rounded-full bg-orange-light opacity-0 transition-opacity"
+    class:opacity-100={mouseDown === 'r'}
+    class:opacity-50={handleHovered === 'r' && !mouseDown}
+  />
+  <!-- left drag handle -->
+  <div
+    class="absolute -left-2 bottom-0 top-0 hidden w-4 cursor-col-resize md:block"
+    on:mousedown={() => (mouseDown = 'l')}
+    on:mouseenter={() => (handleHovered = 'l')}
+    on:mouseleave={() => (handleHovered = false)}
+    role="presentation"
+  />
+  <div
+    class="absolute -left-2 bottom-1 top-1 w-0.5 rounded-full bg-orange-light opacity-0 transition-opacity"
+    class:opacity-100={mouseDown === 'l'}
+    class:opacity-50={handleHovered === 'l' && !mouseDown}
+  />
+
   <slot name="before" {shadow} />
 
   <div
