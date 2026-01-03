d1968135750f630688eaa803cf60de66e85a9dd5
Maxim Dietz <maxim.dietz@goteleport.com>
2026-01-03 00:34:05 -0500

    fix pt spacing

--- svelte-app/src/routes/etc/+page.svelte
+++ svelte-app/src/routes/etc/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="mt-10 flex h-full min-w-full flex-grow flex-col gap-12">
   {#if data.config.about}
-    <section class="flex w-full flex-col">
+    <section class="flex w-full flex-col gap-6">
       <h1 class="font-display flex max-w-2xl flex-col text-5xl tracking-wide">
         About me
       </h1>
@@ -15,7 +15,7 @@
   {/if}
 
   {#if data.config.contact}
-    <section class="flex w-full flex-col">
+    <section class="flex w-full flex-col gap-6">
       <h1 class="font-display flex max-w-2xl flex-col text-5xl tracking-wide">
         Get in touch
       </h1>
