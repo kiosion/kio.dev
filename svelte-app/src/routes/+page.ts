802279fef5ccd3068f191cdb94f00fc315d6f98c
Maxim Dietz <maxim.dietz@goteleport.com>
2026-01-20 10:52:51 -0500

    fixup: w/h calcs for ContentPreview

--- svelte-app/src/routes/+page.ts
+++ svelte-app/src/routes/+page.ts
@@ -1,7 +1,3 @@
-import { unwrapAPIResponse } from '$lib/api/result';
-import { find } from '$lib/api/store';
-import { HOMEPAGE_POSTS_NUM } from '$lib/consts';
-
 import type { PageLoad } from './$types';
 
 export const load = (async ({ parent }) => {
